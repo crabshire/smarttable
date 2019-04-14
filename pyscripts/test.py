170d6221f9bdc7590f6f1f2e621c468deee00ffe
crabshire <cbabshire@gmail.com>
2019-04-13 22:21:42 -0500

    Added buttons to change LED state and exit

--- pyscripts/test.py
+++ pyscripts/test.py
@@ -6,24 +6,24 @@ import time
 import RPi.GPIO as GPIO
 
 def main():
-    loopcontrol = False  
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(18, GPIO.OUT)
     GPIO.setup(23, GPIO.IN)
-
-while not loopcontrol:
-    pressed = GPIO.input(23)
+    GPIO.setup(24, GPIO.IN)
+    GPIO.output(18, False)
     lighton = False
-    if pressed == True:
-        if lighton == True:
-            GPIO.output(18, False)
-            lighton = False
-            pressed = False
-        else:
-            GPIO.output(18, True)
-            lighton = True
-            pressed = False
-
+    while True:
+        if GPIO.input(23):
+            if lighton == True:
+                GPIO.output(18, False)
+                lighton = False
+                time.sleep(1)
+            else:
+                GPIO.output(18, True)
+                lighton = True
+                time.sleep(1)
+        if GPIO.input(24):
+            exit()
 if __name__ == "__main__":
     main()
