e8d20372287b0bd18a96801361626f58dcce61dc
cbabshire <cbabshire@gmail.com>
2019-04-13 21:24:12 -0500

    Fixed formatting

--- pyscripts/test.py
+++ pyscripts/test.py
@@ -6,29 +6,24 @@ import time
 import RPi.GPIO as GPIO
 
 def main():
-    loopcontrol = False   # flag variable to see if they guessed it
+    loopcontrol = False  
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(18, GPIO.OUT)
     GPIO.setup(23, GPIO.IN)
 
 while not loopcontrol:
-        #GPIO.output(18, True)
-        #time.sleep(2)
-        #GPIO.output(18, False)
-        #time.sleep(1)
-	pressed = GPIO.input(23)
-	lighton = False
-	if pressed == True:
-		if lighton == True:
-			GPIO.output(18, False)
-			lighton = False
-			pressed = False
-		else:
-			GPIO.output(18, True)
-			lighton = True
-			pressed = False
-
+    pressed = GPIO.input(23)
+    lighton = False
+    if pressed == True:
+        if lighton == True:
+            GPIO.output(18, False)
+            lighton = False
+            pressed = False
+        else:
+            GPIO.output(18, True)
+            lighton = True
+            pressed = False
 
 if __name__ == "__main__":
     main()
