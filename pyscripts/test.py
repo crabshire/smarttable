c48674425cbd114b9dee628c441dd66cb29568c8
root <root@W1645439.northamerica.cerner.net>
2019-04-15 22:31:04 -0500

    Added Color Changing LED Code

--- pyscripts/test.py
+++ pyscripts/test.py
@@ -4,6 +4,7 @@ This program is my first python test program
 """
 import time
 import RPi.GPIO as GPIO
+import pigpio
 
 def main():
     GPIO.setwarnings(False)
@@ -11,8 +12,16 @@ def main():
     GPIO.setup(17, GPIO.OUT)
     GPIO.setup(27, GPIO.IN)
     GPIO.setup(22, GPIO.IN)
+    GPIO.setup(5, GPIO.OUT)
+    GPIO.setup(6, GPIO.OUT)
+    GPIO.setup(13, GPIO.OUT)
     GPIO.output(17, False)
     lighton = False
+    pi = pigpio.pi()
+    pi.set_PWM_dutycycle(5, 128)
+    pi.set_PWM_dutycycle(6, 128)
+    pi.set_PWM_dutycycle(13, 128)
+    pi.stop()
     while True:
         if GPIO.input(27):
             if lighton == True:
